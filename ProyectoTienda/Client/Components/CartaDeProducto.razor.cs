9ce3324cbd50151d6a05d3b8ff9cf2dc368aabc4
diegoas1205 <diegoas1205@gmail.com>
2020-06-08 11:47:47 -0500

    Añadiendo cartas... considerar usar css grid para las cartas

---
+++ ProyectoTienda/Client/Components/CartaDeProducto.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoTienda.Client.Components
+{
+    public class CartaDeProductoBase:ComponentBase
+    {
+        [Parameter] public string RutaImagenProducto { get; set; }
+        [Parameter] public string NombreProducto { get; set; }
+        [Parameter] public string PrecioOriginalProducto { get; set; }
+        [Parameter] public bool TieneDescuento { get; set; }
+        [Parameter] public string Descuento { get; set; }
+        [Parameter] public string PrecioFinal { get; set; }
+    }
+}
